424b0f4ead3e4cb76bfa1d9e024997aff3bbd313
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-10-24 15:56:05 +1100

    Added APIs to get and create user

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +15,42 @@ namespace UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<UserController>
+        readonly DatabaseContext databaseContext;
+
+        public UserController()
+        {
+            databaseContext = new DatabaseContext();
+        }
+
+        // GET: api/User
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return databaseContext.Users.ToList();
         }
 
-        // GET api/<UserController>/5
+        // GET api/User/id
         [HttpGet("{id}")]
-        public string Get(int id)
+        public User Get(int id)
         {
-            return "value";
+            return databaseContext.Users.Find(id);
         }
 
-        // POST api/<UserController>
+        // POST api/User
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+        public IActionResult Post([FromBody] User model)
+        { 
+            try
+            {
+                databaseContext.Add(model);
+                databaseContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,model);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+                throw;
+            }            
         }
     }
 }
